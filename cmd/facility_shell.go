a2c2298f2e869a1abcbc03b6ca41ff02935c816b
Elijah J. Passmore <elijah@elijahjpassmore.com>
2021-09-27 08:22:07 +1300

    Add styling, check for duplicates
    Signed-off-by: Elijah J. Passmore <elijah@elijahjpassmore.com>

--- cmd/facility_shell.go
+++ cmd/facility_shell.go
@@ -47,9 +47,9 @@ func facilityMessageReceiver() {
 	// TODO: User created? Pass in as argument?
 	// An example setting for form.
 	formSetting := esi.FormSetting{
-		Key: "0",
-		Label: "Do you like apples?: ",
-		Caption: "",
+		Key:         "0",
+		Label:       "Do you like apples?: ",
+		Caption:     "",
 		Placeholder: "Y",
 	}
 	// An example English language form.
@@ -75,18 +75,21 @@ func facilityMessageReceiver() {
 		// Case documentation located at api/esi/deer_facility_service.go.
 		switch x := message.Chunk.(type) {
 		case *esi.FacilityMessage_SendKnownDerFacility:
-			knownFacilities[x.SendKnownDerFacility.FacilityPublicKey] = x.SendKnownDerFacility
+			isInKnownFacilities := false
+			for _, v := range knownFacilities {
+				if v.GetFacilityPublicKey() == x.SendKnownDerFacility.GetFacilityPublicKey() {
+					isInKnownFacilities = true
+				}
+			}
+			if isInKnownFacilities == false {
+				knownFacilities[x.SendKnownDerFacility.FacilityPublicKey] = x.SendKnownDerFacility
+			}
 
 			log.WithFields(log.Fields{
 				"src": msg.Src,
 			}).Info(fmt.Sprintf("Saved facility %s", x.SendKnownDerFacility.GetFacilityPublicKey()))
 
 		case *esi.FacilityMessage_GetDerFacilityRegistrationForm:
-			// TODO: User created? Pass in as argument?
-			log.WithFields(log.Fields{
-				"src": msg.Src,
-			}).Info("Received registration form")
-
 			// Set the basic info.
 			registrationForm.CustomerFacilityPublicKey = msg.Src
 			registrationForm.ProviderFacilityPublicKey = facilityInfo.GetFacilityPublicKey()
@@ -108,7 +111,15 @@ func facilityMessageReceiver() {
 				"src": msg.Src,
 			}).Info("Received registration form")
 
-			receivedRegistrationForms = append(receivedRegistrationForms, *x.SendDerFacilityRegistrationForm)
+			isInForms := false
+			for _, v := range receivedRegistrationForms {
+				if v.GetProviderFacilityPublicKey() == x.SendDerFacilityRegistrationForm.GetProviderFacilityPublicKey() {
+					isInForms = true
+				}
+			}
+			if isInForms == false {
+				receivedRegistrationForms = append(receivedRegistrationForms, *x.SendDerFacilityRegistrationForm)
+			}
 
 		case *esi.FacilityMessage_SubmitDerFacilityRegistrationForm:
 			// TODO: Fill in registration form.
@@ -151,7 +162,7 @@ func facilityMessageReceiver() {
 func facilityInputReceiver() {
 	shell := ishell.New()
 	<-facilityClient.OnConnect.C
-	shell.Printf("Connection opened on facility '%s'\n", facilityInfo.GetName())
+	shell.Printf("Connection opened on facility '%s'\n", infoMsgColorFunc(facilityInfo.GetName()))
 
 	shell.AddCmd(&ishell.Cmd{
 		Name: "public",
@@ -167,7 +178,7 @@ func facilityInputReceiver() {
 		Func: func(c *ishell.Context) {
 			if len(knownFacilities) > 0 {
 				for _, v := range knownFacilities {
-					shell.Printf("\nName: %s\nCountry: %s\nPublic Key: %s\n", v.GetName(), v.Location.GetCountry(), v.GetFacilityPublicKey())
+					shell.Printf("\nName: %s\nCountry: %s\nPublic Key: %s\n", v.GetName(), v.Location.GetCountry(), noteMsgColorFunc(v.GetFacilityPublicKey()))
 				}
 				shell.Println()
 			}
@@ -192,30 +203,13 @@ func facilityInputReceiver() {
 		Help: "query registry for facilities by location",
 		Func: func(c *ishell.Context) {
 			// TODO: Ask for more than just country.
+			// TODO: Evaluate settings.
 			c.Print("Registry Public Key: ")
 			registryPublicKey := c.ReadLine()
 			c.Print("Country: ")
 			country := c.ReadLine()
-
-			// Currently, doesn't make a difference.
-			programTypes := []string{
-				"SPINNING RESERVE",
-				"FREQUENCY REGULATION",
-				"RAMPING",
-				"ARTIFICIAL INERTIA",
-				"VOLTAGE MANAGEMENT",
-				"PEAK CAPACITY MANAGEMENT",
-				"CONTRACTUAL OBLIGATIONS",
-				"MARKET PRICE RESPONSE",
-			}
-			programChoices := c.Checklist(programTypes, "Press SPACE to select desired programs", nil)
-
 			newLocation := esi.Location{
 				Country: country,
-			}
-			newPrograms := []esi.DerProgramType(nil)
-			for _, v := range programChoices {
-				newPrograms = append(newPrograms, esi.DerProgramType(v))
 			}
 
 			request := esi.DerFacilityExchangeRequest{Location: &newLocation}
@@ -253,7 +247,7 @@ func facilityInputReceiver() {
 		Func: func(c *ishell.Context) {
 			if len(receivedRegistrationForms) > 0 {
 				for _, v := range receivedRegistrationForms {
-					shell.Printf("\nProvider Public Key: %s\n", v.GetProviderFacilityPublicKey())
+					shell.Printf("\nProvider Public Key: %s\n", noteMsgColorFunc(v.GetProviderFacilityPublicKey()))
 				}
 				shell.Println()
 			}
@@ -270,6 +264,7 @@ func facilityInputReceiver() {
 			signed := false
 			for i, v := range receivedRegistrationForms {
 				if v.GetProviderFacilityPublicKey() == facilityPublicKey {
+					// TODO: nonce
 					data := esi.DerFacilityRegistrationFormData{
 						CustomerFacilityPublicKey: facilityInfo.GetFacilityPublicKey(),
 					}
@@ -282,9 +277,9 @@ func facilityInputReceiver() {
 					signed = true
 
 					// Remove form from list.
-					receivedRegistrationForms[i] = receivedRegistrationForms[len(receivedRegistrationForms) - 1]
-					receivedRegistrationForms[len(receivedRegistrationForms) - 1] = esi.DerFacilityRegistrationForm{}
-					receivedRegistrationForms = receivedRegistrationForms[:len(receivedRegistrationForms) - 1]
+					receivedRegistrationForms[i] = receivedRegistrationForms[len(receivedRegistrationForms)-1]
+					receivedRegistrationForms[len(receivedRegistrationForms)-1] = esi.DerFacilityRegistrationForm{}
+					receivedRegistrationForms = receivedRegistrationForms[:len(receivedRegistrationForms)-1]
 
 					log.WithFields(log.Fields{
 						"end": v.GetProviderFacilityPublicKey(),
